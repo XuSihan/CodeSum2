8a44193edcbb1d5b088a0b5683db9bc6970468f1
XuSihan <x.sihan@foxmail.com>
2018-05-17 10:42:58 +0800

    simple_seq2seq_learner

--- simple_seq2seq_learner.py
+++ simple_seq2seq_learner.py
@@ -1,5 +1,5 @@
 from __future__ import print_function
-from seq2seq import *
+from seq2seq.models import SimpleSeq2Seq
 from generate_datasets import DataGenerator
 import sys
 
@@ -8,16 +8,29 @@ class SimpleSeq2SeqLearner(object):
 		self.hyperparameters = hyperparameters
 		self.parameters = None	
 		self.naming_data = None
+		self.test_name = None
+		self.test_code = None
+		self.__check_all_hyperparmeters_exist()
 
-	def train(self, input_file, max_name_size=10, max_code_size=300, pct_train=0.65, pct_val=0.05, pct_test=.3, patience=5, max_epochs=1000):
+	def train(self, input_file, pct_train=0.65, pct_val=0.05, pct_test=.3, patience=5, max_epochs=1000):
+		model = SimpleSeq2Seq(self.hyperparameters)
+		model.compile(loss='mse', optimizer='rmsprop')
+
 		assert self.parameters == None, ("The model has already been trained!")
 		train_data, val_data, test_data, self.naming_data = DataGenerator.get_data_for_simple_seq2seq_with_validation_and_test(input_file, max_name_size, max_code_size, pct_train, pct_val, pct_test)
+		train_name, train_code = train_data
+		val_name, val_code = val_data
+		self.test_name, self.test_code = test_data
 		
+	def __check_all_hyperparmeters_exist(self):
+		all_params = ["output_dim", "output_length"]
 
+		for param in all_params:
+			assert param in self.hyperparameters, param
+			
 if __name__ == '__main__':
 	if len(sys.argv) > 1:
 		filepath = sys.argv[1]
-		test = DataGenerator(filepath)
 		'''	
 		with open ('names.txt','w') as f:
 			for name in test.names:
