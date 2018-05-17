249a1e84cb537e9951267aa22ee7b1572df3a239
XuSihan <x.sihan@foxmail.com>
2018-05-17 11:27:05 +0800

    simple

--- simple_seq2seq_learner.py
+++ simple_seq2seq_learner.py
@@ -10,17 +10,27 @@ class SimpleSeq2SeqLearner(object):
 		self.naming_data = None
 		self.test_name = None
 		self.test_code = None
+		self.model = None
 		self.__check_all_hyperparmeters_exist()
 
 	def train(self, input_file, pct_train=0.65, pct_val=0.05, pct_test=.3, patience=5, max_epochs=1000):
-		model = SimpleSeq2Seq(self.hyperparameters)
-		model.compile(loss='mse', optimizer='rmsprop')
-
 		assert self.parameters == None, ("The model has already been trained!")
-		train_data, val_data, test_data, self.naming_data = DataGenerator.get_data_for_simple_seq2seq_with_validation_and_test(input_file, max_name_size, max_code_size, pct_train, pct_val, pct_test)
+		assert "input_length" in self.hyperparameters, ('input_length')
+		train_data, val_data, test_data, self.naming_data = DataGenerator.get_data_for_simple_seq2seq_with_validation_and_test(input_file, self.hyperparameters['output_length'], self.hyperparameters['input_length'], pct_train, pct_val, pct_test)
 		train_name, train_code = train_data
 		val_name, val_code = val_data
 		self.test_name, self.test_code = test_data
+		self.hyperparameters['n_tokens'] = self.naming_data.all_tokens_dictionary.get_n_tokens()
+		model = SimpleSeq2Seq(**self.hyperparameters)
+		model.compile(loss='mse', optimizer='rmsprop')
+		self.model = model
+		
+		
+		model.fit(train_code, train_name)
+		print ('predict...')
+		print (model.predict(val_name))
+		print ('evaluate...')
+		print (model.evaluate(val_code,val_name))
 		
 	def __check_all_hyperparmeters_exist(self):
 		all_params = ["output_dim", "output_length"]
@@ -39,7 +49,6 @@ if __name__ == '__main__':
 			for code in test.codes:
 				f.write(' '.join(code) + '\n')
 		'''
-		id_names, id_codes, vocabulary_size = test.get_data_for_basic_seq2seq(300,10)
-		print ('id_names.shape: ', id_names.shape)
-		print ('id_codes.shape: ', id_codes.shape)
-		print ('vocabulary_size: ', vocabulary_size)
+		params = dict(output_dim=128,output_length=8, input_length=300, is_embedding=False)
+		model = SimpleSeq2SeqLearner(params)
+		model.train(filepath)
